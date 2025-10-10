32a78925917fa08e9d6fb26a46155246d7000dff
fluffypal <fluffypal17@gmail.com>
2025-10-10 17:54:40 +0300

    Added: nvidia driver in flake.nix nixosConfiguretions and File Orgnaayz

--- flake.nix
+++ flake.nix
@@ -5,21 +5,44 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs, ... }: {
-    nixosConfigurations.fluffypal = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./user/fluffypal/fluffypal.nix
-        ./configuration.nix
-        ./modules/network/network.nix
-        ./modules/network/dns.nix
-        ./modules/network/firewall.nix
-        ./modules/desktop/gnome/gnome.nix
-        ./modules/desktop/gnome/autologin.nix
-        ./modules/system/boot/boot.nix
-        ./modules/system/locale/locale.nix
-        ./modules/system/xserver/xserver.nix
-      ];
+  outputs = { self, nixpkgs, ... }:
+  let
+    inherit (nixpkgs.lib) nixosSystem;
+    fluffypalModules = [
+      ./user/fluffypal/fluffypal.nix
+      ./configuration.nix
+      ./modules/network/network.nix
+      ./modules/network/dns.nix
+      ./modules/network/firewall.nix
+      ./modules/desktop/gnome/gnome.nix
+      ./modules/desktop/gnome/autologin.nix
+      ./modules/system/boot/boot.nix
+      ./modules/system/locale/locale.nix
+      ./modules/system/xserver/xserver.nix
+    ];
+  in
+  {
+    nixosConfigurations = {
+      fluffypal = nixosSystem {
+        system = "x86_64-linux";
+        modules = fluffypalModules;
+      };
+      fluffypal-nvidia = nixosSystem {
+        system = "x86_64-linux";
+        modules = fluffypalModules ++ [ ./modules/hardware/nvidia/nvidia.nix ];
+      };
+      fluffypal-nvidia-open = nixosSystem {
+        system = "x86_64-linux";
+        modules = fluffypalModules ++ [ ./modules/hardware/nvidia/nvidia-open-kernel.nix ];
+      };
+      fluffypal-nvidia-turing = nixosSystem {
+        system = "x86_64-linux";
+        modules = fluffypalModules ++ [ ./modules/hardware/nvidia/nvidia-turing.nix ];
+      };
+      fluffypal-nvidia-open-turing = nixosSystem {
+        system = "x86_64-linux";
+        modules = fluffypalModules ++ [ ./modules/hardware/nvidia/nvidia-turing-open-kernel.nix ];
+      };
     };
   };
 }
